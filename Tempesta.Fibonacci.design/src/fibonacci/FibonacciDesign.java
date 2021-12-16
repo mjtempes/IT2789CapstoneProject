c63dba87c07b2f1d77854c7c15d00c393b8b2fc2
Michael Tempesta <mjtempes@gmail.com>
2021-12-16 09:55:46 -0500

    Final

--- Tempesta.Fibonacci.design/src/fibonacci/FibonacciDesign.java
+++ Tempesta.Fibonacci.design/src/fibonacci/FibonacciDesign.java
@@ -5,11 +5,20 @@ public class FibonacciDesign
 
 	public FibonacciCaculation()
 	{
+		// Calls the Calculation method.
+	}
+	
+	public void Calulation()
+	{
 		
 		//Calculate the first Fibonacci number.
 		//store the number that is calculated.
 		//Calculate the second number using the stored number.
-		//display the results.
+		
+	}
+	public void calcResults(int firstTerm)
+	{
+		// Display the results
 	}
 
 }
