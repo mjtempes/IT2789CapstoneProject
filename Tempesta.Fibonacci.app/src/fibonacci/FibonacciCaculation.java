0377cf35f482d831b44c056dd311a2a03046afff
Michael Tempesta <mjtempes@gmail.com>
2021-12-15 22:54:34 -0500

    final

--- Tempesta.Fibonacci.app/src/fibonacci/FibonacciCaculation.java
+++ Tempesta.Fibonacci.app/src/fibonacci/FibonacciCaculation.java
@@ -4,23 +4,34 @@ public class FibonacciCaculation
 {
 	public FibonacciCaculation()
 	{
-		//Variables defined
-		int mathnumber = 50;
+		calculation();
+	}
+
+	public void calculation()
+	{
+		// Variables defined
+		int mathnumber = 45;
 		int firstTerm = 0;
 		int secondTerm = 1;
+		
 		// Displayed the line to tell user what is to follows.
 		System.out.print("Fibonacci Series first " + mathnumber + " terms:\n\n");
 
-		for (int i = 1; i <= mathnumber; ++i)
+		for (int Output = 1; Output <= mathnumber; ++Output)
 		{
-			//Display the first 50 numbers in the sequence
-			System.out.print(firstTerm +"\t");
-		
+			calcResults(firstTerm);
+
 			// compute the next term
 			int mathcalc = firstTerm + secondTerm;
 			firstTerm = secondTerm;
 			secondTerm = mathcalc;
 
 		}
+	}
+
+	public void calcResults(int firstTerm)
+	{
+		// Display the first 45 numbers in the sequence
+		System.out.print(firstTerm + "\n\n");
 	}
 }
